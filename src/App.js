0eae3af5b1a9f7775bd53c1a2664739548226e69
ihssenelaini <ihssenelaini@outlook.fr>
2019-02-19 11:21:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Chrono from './chrono'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+    <Chrono/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/chrono.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+
+class Chrono extends  Component {
+    render() {
+      return (
+        <div className="App">
+          <h1>00:30:00</h1>
+          <h5>hour minute second </h5>
+          
+        </div>
+      );
+    }
+  }
+  
+  export default Chrono;
